88a707b0a5f94c3cb5c9148a3cb84dc9f7f858b0
vas89 <34172250+vas89@users.noreply.github.com>
2025-06-22 02:10:56 +0300

    Create catalog.js

---
+++ catalog.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: "Тормозные колодки Bosch",
+    price: 3200,
+    image: "https://images.unsplash.com/photo-1605559424843-3604860a6d2b"
+  },
+  {
+    name: "Фильтр воздушный Mann",
+    price: 1500,
+    image: "https://images.unsplash.com/photo-1616348436168-c1dbe0a9019b"
+  },
+  {
+    name: "Аккумулятор Varta 60Ah",
+    price: 7800,
+    image: "https://images.unsplash.com/photo-1617290813364-7be3f3de8b8b"
+  }
+];
